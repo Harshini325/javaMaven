8d5a8bcf644b3d5c4572edb1eb3b0d615f38d839
Harshini325 <sriharshinichintalapudi@gmail.com>
2025-11-24 08:35:32 +0530

    Initial commit

---
+++ src/main/java/com/example/meghMaven/App.java
@@ -0,0 +1,10 @@
+package com.example.meghMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! this is meghana");
+    }
+}
